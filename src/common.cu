cfb94b8deca78dc86bc068058ca9b3388dfd2a98
yzh1703456 <yzh1703456@163.com>
2025-04-15 21:55:32 +0800

    add build

--- src/common.cu
+++ src/common.cu
@@ -281,13 +281,17 @@ testResult_t testStreamSynchronize(int ngpus, cudaStream_t* streams, ncclComm_t*
   memset(done, 0, sizeof(int)*ngpus);
   timer tim;
 
+  printf("-----------NCCL TESTS: common.cu, testStreamSynchronize: 284-----------\n");
+
   while (remaining) {
+  printf("-----------NCCL TESTS: common.cu, testStreamSynchronize, in while: 287-----------\n");
    int idle = 1;
    for (int i=0; i<ngpus; i++) {
      if (done[i]) continue;
-
+     printf("-----------NCCL TESTS: common.cu, testStreamSynchronize, in for: 291-----------\n");
      cudaErr = cudaStreamQuery(streams[i]);
      if (cudaErr == cudaSuccess) {
+       printf("-----------NCCL TESTS: common.cu, testStreamSynchronize, in if: 294-----------\n");
        done[i] = 1;
        remaining--;
        idle = 0;
@@ -328,6 +332,9 @@ testResult_t testStreamSynchronize(int ngpus, cudaStream_t* streams, ncclComm_t*
    // We might want to let other threads (including NCCL threads) use the CPU.
    if (idle) sched_yield();
   }
+
+  printf("-----------NCCL TESTS: common.cu, testStreamSynchronize: 334-----------\n");
+  
   free(done);
   return testSuccess;
 }
